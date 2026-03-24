a11bcd25dbfe188de87dad7e5b95a1ff69273dcd
Tomas Jirka <tomas.jirka@catonetworks.com>
2026-03-24 17:44:09 +0100

    example

--- examples/resources/cato_private_access_rule/resource.tf
+++ examples/resources/cato_private_access_rule/resource.tf
@@ -37,7 +37,7 @@ locals {
           }
         },
         "source": {
-          "user": [
+          "users": [
             { "name": "test user_1" },
             { "name": "test user_2" }
           ],
