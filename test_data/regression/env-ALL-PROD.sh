c280a85e1216f31cd3d74cd1b48a81895cf11560
Tomas Jirka <tomas.jirka@catonetworks.com>
2026-06-19 19:35:00 +0200

    env files for regression tests

---
+++ test_data/regression/env-ALL-PROD.sh
@@ -0,0 +1,37 @@
+# Acceptance test variables for PROD account 18759
+export TFACC_TEST_VARS='{
+  "global_ip_ranges": [
+    { "name": "global_ip_range", "id": "2961658" },
+    { "name": "global_ip_range_2", "id": "2961659" },
+    { "name": "global_ip_range_3", "id": "3087731" }
+  ],
+  "floating_ranges": [
+    { "name": "floating_range", "id": "2961665" },
+    { "name": "floating_range_2", "id": "2961666" },
+    { "name": "floating_range_3", "id": "3087745" }
+  ],
+  "user_groups": [
+    { "name": "user_group_1", "id": "500000006" },
+    { "name": "user_group_2", "id": "500000007" },
+    { "name": "user_group_3", "id": "500000008" }
+  ],
+  "system_groups": [
+    { "name": "All Users", "id": "11S" },
+    { "name": "All SDP Users", "id": "2S" }
+  ],
+  "device_postures": [
+    { "name": "Test Device Posture Profile", "id": "10624" },
+    { "name": "Test device posture 1", "id": "11132" },
+    { "name": "Test device posture 2", "id": "11133" }
+  ],
+  "custom_apps": [
+    { "name": "acctest_custom_app_1", "id": "45aa547b-1725-43b0-b89f-fe95ade6204d" },
+    { "name": "acctest_custom_app_2", "id": "0ae9ba55-68c5-4d4a-bceb-da14d7ed4289" },
+    { "name": "acctest_custom_app_3", "id": "f352313d-02ef-400c-86f2-674b51db38d9" }
+  ],
+  "custom_categories": [
+    { "name": "acctest_custom_category_1", "id": "44284" },
+    { "name": "acctest_custom_category_2", "id": "44285" },
+    { "name": "acctest_custom_category_3", "id": "44286" }
+  ]
+}'
---
+++ test_data/regression/env-ALL-STA.sh
@@ -0,0 +1,38 @@
+# Acceptance test variables for STG account 62440
+export TFACC_TEST_VARS='{
+  "global_ip_ranges": [
+    { "name": "global_ip_range", "id": "1305171" },
+    { "name": "global_ip_range_2", "id": "1305172" },
+    { "name": "global_ip_range_3", "id": "1401240" }
+  ],
+  "floating_ranges": [
+    { "name": "floating_range", "id": "1305173" },
+    { "name": "floating_range_2", "id": "1305174" },
+    { "name": "floating_range_3", "id": "1401245" }
+  ],
+  "user_groups": [
+    { "name": "user_group_1", "id": "500000016" },
+    { "name": "user_group_2", "id": "500000017" },
+    { "name": "user_group_3", "id": "500000019" }
+  ],
+  "system_groups": [
+    { "name": "All Floating Ranges", "id": "7S" },
+    { "name": "All SDP Users", "id": "2S" },
+    { "name": "All LDAP Users", "id": "10000010S" }
+  ],
+  "device_postures": [
+    { "name": "Test Device Posture Profile", "id": "445784" },
+    { "name": "Test device posture 1", "id": "463096" },
+    { "name": "Test device posture 2", "id": "473896" }
+  ],
+  "custom_apps": [
+    { "name": "acctest_custom_app_1", "id": "3bc976fc-4f77-4f88-837c-ec73e9de8f9d" },
+    { "name": "acctest_custom_app_2", "id": "d86a1e1e-1ae4-48c3-a40b-031cd95b62c2" },
+    { "name": "acctest_custom_app_3", "id": "af990dd3-afcf-4bb5-9ccb-7813fbfd0201" }
+  ],
+  "custom_categories": [
+    { "name": "acctest_custom_category_1", "id": "74382" },
+    { "name": "acctest_custom_category_2", "id": "74383" },
+    { "name": "acctest_custom_category_3", "id": "74477" }
+  ]
+}'
