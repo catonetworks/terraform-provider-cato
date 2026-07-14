2c5406ef602ba0a7f7aefa3c4068111d26f94161
Tomas Jirka <tomas.jirka@email.cz>
2026-07-14 15:46:07 +0200

    add regression test env settings (#111)

---
+++ test_data/regression/env-NETWORK-TEST.sh
@@ -0,0 +1,32 @@
+# Acceptance test variables for TEST account 248225
+export TFACC_TEST_VARS='{
+  "floating_ranges": [
+    { "name": "floating_range_1", "id": "2272570" },
+    { "name": "floating_range_2", "id": "2272569" },
+    { "name": "floating_range_3", "id": "2272568" }
+  ],
+  "user_groups": [
+    { "name": "user_group_1", "id": "500000000" },
+    { "name": "user_group_2", "id": "500000002" },
+    { "name": "user_group_3", "id": "500000003" }
+  ],
+  "system_groups": [
+    { "name": "All Floating Ranges", "id": "7S" },
+    { "name": "All Sites", "id": "1S" }
+  ],
+  "device_postures": [
+    { "name": "device_posture_1", "id": "40751" },
+    { "name": "device_posture_2", "id": "40752" },
+    { "name": "device_posture_3", "id": "40753" }
+  ],
+  "custom_apps": [
+    { "name": "custom_app_1", "id": "86baea1a-345f-4cbd-b506-90c16b3cfaea" },
+    { "name": "custom_app_2", "id": "5da7ca54-0e34-4c1f-a61a-8ce838b50e40" },
+    { "name": "custom_app_3", "id": "3cb02bb6-dadf-4c4b-b8fb-40da5e4a9f14" }
+  ],
+  "custom_categories": [
+    { "name": "custom_category_1", "id": "344855" },
+    { "name": "custom_category_2", "id": "344856" },
+    { "name": "custom_category_3", "id": "344857" }
+  ]
+}'
