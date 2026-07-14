614bfac5f31fd57cb1af020e369a099d8d9a42f1
Ilya Nikitin <ilya.nikitin@catonetworks.com>
2026-07-14 15:56:04 +0200

    ci: retry transient API failures

--- test_data/flaky_acctest.sh
+++ test_data/flaky_acctest.sh
@@ -53,7 +53,7 @@ run_test() {
 }
 
 should_retry() {
-	grep -E '(internal server error|connection refused|message\\":\\"Internal server\\n")' "$1" > /dev/null && return 0
+	grep -Ei '(internal server error|connection (error|refused)|DOWNSTREAM_SERVICE_ERROR|message\\":\\"Internal server\\n")' "$1" > /dev/null && return 0
 	return 1
 }
 retry_test() {
